316182568c724242a055ceee04249cabf33341c1
Chris Ha <chunghha@users.noreply.github.com>
2018-10-27 10:28:25 -0500

    feat: ng add @angular/pwa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,8 @@ import { NgZorroAntdMobileModule } from 'ng-zorro-antd-mobile';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -19,7 +21,8 @@ import { AppComponent } from './app.component';
     HttpClientModule,
     IonicModule.forRoot(),
     NgZorroAntdMobileModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
   ],
   providers: [
     StatusBar,
